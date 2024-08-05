eb2d073fffbadccade446390c2d00332800b5e2d
AryanDaksh <167518113+AryanDaksh27@users.noreply.github.com>
2024-08-05 10:06:00 +0530

    Push

---
+++ src/components/socialicons/index.js
@@ -0,0 +1,65 @@
+import React from "react";
+import "./style.css";
+import {
+  FaGithub,
+  FaTwitter,
+  FaFacebookF,
+  FaLinkedin,
+  FaTwitch,
+  FaInstagram,
+} from "react-icons/fa";
+import { socialprofils } from "../../content_option";
+
+export const Socialicons = (params) => {
+  return (
+    <div className="stick_follow_icon">
+      <ul>
+      {socialprofils.linkedin && (
+          <li>
+            <a href={socialprofils.linkedin}>
+              <FaLinkedin />
+            </a>
+          </li>
+        )}
+        {socialprofils.github && (
+          <li>
+            <a href={socialprofils.github}>
+              <FaGithub />
+            </a>
+          </li>
+        )}
+        {socialprofils.twitter && (
+          <li>
+            <a href={socialprofils.twitter}>
+              <FaTwitter />
+            </a>
+          </li>
+        )}
+        
+        {socialprofils.facebook && (
+          <li>
+            <a href={socialprofils.facebook}>
+              <FaFacebookF />
+            </a>
+          </li>
+        )}
+        
+        {socialprofils.instagram && (
+          <li>
+            <a href={socialprofils.instagram}>
+              <FaInstagram />
+            </a>
+          </li>
+        )}
+        {socialprofils.twitch && (
+          <li>
+            <a href={socialprofils.twitch}>
+              <FaTwitch />
+            </a>
+          </li>
+        )}
+      </ul>
+      <p>Contact Me</p>
+    </div>
+  );
+};
---
+++ src/content_option.js
@@ -0,0 +1,133 @@
+const logotext = "ARYAN";
+const meta = {
+    title: "Aryan Daksh",
+    description: "#",
+};
+
+const introdata = {
+    title: "Hello! My name is Aryan Daksh",
+    animated: {
+        first: "Currently pursuing Computer Science in Manipal University Jaipur",
+        second: "Fluent in Programming Languages like Java and Python",
+        third: "Let's Collaborate on Cutting-Edge Technologies",
+    },
+    description: "A passionate Computer Science major with a deep interest in Software Development and Cloud Computing.",
+    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+    title: "A bit about myself:",
+    aboutme: "I thrive on tackling new challenges and am always eager to explore the latest advancements in technology. My technical toolkit includes proficiency in Python, Java and cloud platforms such as AWS and Azure, along with a solid foundation in Data Structures and Object-Oriented Programming. ",
+};
+const worktimeline = [{
+        jobtitle: "Undergraduate Research Assistant",
+        where: "Manipal University Jaipur",
+        date: "Jan 2024 - May 2024",
+    },
+    {
+        jobtitle: "Software Development Intern",
+        where: "RateGain",
+        date: "June 2024 - September 2024",
+    },
+    
+];
+
+const skills = [
+    {
+        name: "Java",
+        value: 90,
+    },
+    {
+        name: "SpringBoot",
+        value: 95,
+    },
+    {
+        name: "Python",
+        value: 80,
+    },
+    
+    {
+        name: "AWS",
+        value: 70,
+    },
+    {
+        name: "React",
+        value: 70,
+    },
+    
+];
+
+const services = [{
+        title: "Web Application Development",
+        description: "Create responsive, dynamic web applications using modern frameworks and technologies.",
+    },
+    {
+        title: "Cloud Architecture Design",
+        description: "Ability to design scalable, reliable, and cost-effective cloud solutions using AWS, Azure, or other cloud platforms.",
+    },
+    {
+        title: "API Development and Integration",
+        description: "Design and implement APIs for seamless integration with other systems and services.",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://picsum.photos/400/?grayscale",
+        description: "Student Data Management System Using SpringBoot",
+        link: "https://github.com/AryanDaksh27/Student-Data-Management-System-using-Spring",
+    },
+    {
+        img: "https://picsum.photos/400/800/?grayscale",
+        description: "Detecting Trojan Traffic Using Machine Learning Algorithms",
+        link: "https://github.com/AryanDaksh27/Trojan-Traffic-Detector",
+    },
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "Face Recognition Based Attendance System",
+        link: "https://github.com/AryanDaksh27/Face-Detection",
+    },   
+    {
+        img: "https://picsum.photos/400/400?grayscale",
+        description: "Detecting Fake News Using Binary Classification",
+        link: "https://github.com/AryanDaksh27/Fake-News-Classification",
+    },
+    {
+        img: "https://picsum.photos/400/500/?grayscale",
+        description: "Fuzzy Systems For A Food Cooking Problem",
+        link: "https://github.com/AryanDaksh27/Cooking-Problem-Fuzzy-System",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "My Portfolio Website",
+        link: "#",
+    },
+
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "aryandaksh2711@gmail.com.com",
+    YOUR_FONE: "+91-9354141341",
+    description: "I'd love to hear from you. Whether you have a question, a project idea, feel free to reach out. Looking forward to connecting with you!",
+    YOUR_SERVICE_ID: "service_w09gsfw",
+    YOUR_TEMPLATE_ID: "template_0rg8wtd",
+    YOUR_USER_ID: "PSso4eALXlw-fb5xz",
+};
+
+const socialprofils = {
+    github: "https://github.com/AryanDaksh27",
+    instagram: "https://www.instagram.com/aryandaksh_/",
+    linkedin: "https://www.linkedin.com/in/aryan-daksh-8b2a4223b/",
+    twitter: "https://x.com/Abcde1201501196",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
